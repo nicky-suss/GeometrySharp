64304ba314480a2c8bc325bf5e3cec7daf087ecf
nicky_sus <gfmm2558@gmail.com>
2026-05-04 15:22:22 +0300

    Add Cube class and its methods

--- src/SolidGeometry.cs
+++ src/SolidGeometry.cs
@@ -2,5 +2,9 @@ namespace GeoSharp;
 
 public static class SolidGeometry
 {
-    
+    public static class Cube
+    {
+        public static double GetVolume(double a) => a * a * a;
+        public static double GetSurfaceArea(double a) => (a * a) * 6;
+    }
 }
