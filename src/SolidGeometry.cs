5584251774cbfe4a9303099f96f7f6289cbb1a1d
nicky_sus <gfmm2558@gmail.com>
2026-05-04 15:27:25 +0300

    Add Sphere class and its methods

--- src/SolidGeometry.cs
+++ src/SolidGeometry.cs
@@ -7,4 +7,9 @@ public static class SolidGeometry
         public static double GetVolume(double a) => a * a * a;
         public static double GetSurfaceArea(double a) => (a * a) * 6;
     }
+    public static class Sphere
+    {
+        public static double GetVolume(double r) => ((r * r * r) * Math.PI) * (4.0 / 3.0);
+        public static double GetSurfaceArea(double r) => ((r * r) * Math.PI) * 4;
+    }
 }
