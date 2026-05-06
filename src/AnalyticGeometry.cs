b87847aaa7ee8b2803fdcb790e47c93afd503083
nicky_sus <gfmm2558@gmail.com>
2026-05-06 14:57:24 +0300

    Add GetMidpoint method and fixed GetDistance method

--- src/AnalyticGeometry.cs
+++ src/AnalyticGeometry.cs
@@ -14,16 +14,30 @@ public static class AnalyticGeometry
         /// 
         /// </summary>
         /// <param name="xOne"></param>
-        /// <param name="xTwo"></param>
         /// <param name="yOne"></param>
+        /// <param name="xTwo"></param>
         /// <param name="yTwo"></param>
         /// <returns></returns>
-        public static double GetDistance(double xOne, double xTwo, double yOne, double yTwo)
+        public static double GetDistance(double xOne, double yOne, double xTwo, double yTwo)
         {
             double dx = xTwo - xOne;
             double dy = yTwo - yOne;
             double xy = (dx * dx) + (dy * dy);
             return Math.Sqrt(xy);
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="xOne"></param>
+        /// <param name="yOne"></param>
+        /// <param name="xTwo"></param>
+        /// <param name="yTwo"></param>
+        /// <returns></returns>
+        public static (double x, double y) GetMidpoint(double xOne, double yOne, double xTwo, double yTwo)
+        {
+            double midX = (xOne + xTwo) / 2;
+            double midY = (yOne + yTwo) / 2;
+            return (midX, midY);
         }
     }
 }
