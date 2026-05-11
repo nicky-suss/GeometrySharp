9643095abd1ddc5abc533d4f9106564a521e43c2
nicky_sus <gfmm2558@gmail.com>
2026-05-11 11:25:10 +0300

    Add TriangleRelations class and a method GetHypotenuse

--- src/Trigonometry.cs
+++ src/Trigonometry.cs
@@ -7,4 +7,8 @@ public static class Trigonometry
         public static double ToRadians(double degrees) => degrees * (Math.PI / 180);
         public static double ToDegrees(double radians) => radians * (180 / Math.PI);
     }
+    public static class TriangleRelations
+    {
+        public static double GetHypotenuse(double a, double b) => double.Hypot(a, b);
+    }
 }
