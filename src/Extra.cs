17ecf7e8f320df70016eda8fd36fb2c2f5404972
nicky_sus <gfmm2558@gmail.com>
2026-05-13 20:31:26 +0300

    Add more methods

--- src/Extra.cs
+++ src/Extra.cs
@@ -11,4 +11,12 @@ internal static class Extra
         throw new DivideByZeroException("The slope is undefined for a vertical line.");
     internal static void ThrowIdenticalPoints() =>
         throw new ArgumentException("Both points are identical. Operation requires two distinct points.");
+    internal static void ThrowInvalidHypotenuse() =>
+        throw new ArgumentException("Hypotenuse must be greater than the known leg.");
+    internal static void ThrowInvalidAngles() =>
+        throw new ArgumentException("The sum of the angles must be less than 180 degrees, and each angle must be positive.");
+    internal static void ThrowInvalidTriangleAngle() =>
+        throw new ArgumentException("Angle must be between 0 and 180 degrees");
+    internal static void ThrowInvalidDimension() =>
+        throw new ArgumentException("Side length must be greater than zero.");
 }
