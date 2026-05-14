2614e50babd38ca83ce72f8f04f61af57093bcbc
nicky_sus <gfmm2558@gmail.com>
2026-05-14 18:53:12 +0300

    Add GetMagnitude and GetMagnitude3D methods

--- src/VectorMath.cs
+++ src/VectorMath.cs
@@ -25,4 +25,19 @@ public static class VectorMath
     /// <param name="zTwo">The Z-component of the second vector.</param>
     /// <returns>The scalar dot product of the two 3D vectors.</returns>
     public static double GetDotProduct3D(double xOne, double yOne, double zOne, double xTwo, double yTwo, double zTwo) => (xOne * xTwo) + (yOne * yTwo) + (zOne * zTwo);
+    /// <summary>
+    /// Calculates the magnitude (length) of a 2D vector.
+    /// </summary>
+    /// <param name="x">The X-component of the vector.</param>
+    /// <param name="y">The Y-component of the vector.</param>
+    /// <returns>The magnitude of the 2D vector.</returns>
+    public static double GetMagnitude(double x, double y) => Math.Sqrt((x * x) + (y * y));
+    /// <summary>
+    /// Calculates the magnitude (length) of a 3D vector.
+    /// </summary>
+    /// <param name="x">The X-component of the vector.</param>
+    /// <param name="y">The Y-component of the vector.</param>
+    /// <param name="z">The Z-component of the vector.</param>
+    /// <returns>The magnitude of the 3D vector.</returns>
+    public static double GetMagnitude3D(double x, double y, double z) => Math.Sqrt((x * x) + (y * y) + (z * z));
 }
